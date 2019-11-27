a3aef8fcb945f8d659d2cfba19f545dbfdfe18ad
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-11-27 23:20:59 +0000

    Added comments to UpdateInfoController

--- src/app/controller/UpdateInfoController.java
+++ src/app/controller/UpdateInfoController.java
@@ -18,53 +18,66 @@ public class UpdateInfoController implements ActionListener {
 	private UpdateInfo updateInfo;
 	private MainViewController mainViewController;
 	private MainView mainView;
-	
+
+	// DEFAULT CONSTRUCTOR
 	public UpdateInfoController(UpdateInfo updateInfo, MainViewController mainViewController, MainView mainView) {
 		this.updateInfo = updateInfo;
 		this.mainViewController = mainViewController;
 		this.mainView = mainView;
 	}
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
+		// ACTION LISTENER FOR CANCELLING UPDATE
 		if (e.getActionCommand().equals("Cancel")) {
 			this.updateInfo.dispose();
 		}
-		
+
+		// ACTION LISTENER FOR UPDATING CANDIDATE INFORMATION
 		if (e.getActionCommand().equals("Update")) {
-			
+
+			// VALIDATION FOR PASSPORT, NAME AND SURNAME - NO NEED FOR PRIORITY AND CALENDAR
 			if (this.mainViewController.registerValidation(this.updateInfo, this.updateInfo.getPassport().getText(),
-					this.mainViewController.getRegexPassport(), this.updateInfo.getNameS().getText(), 
-						this.mainViewController.getRegexGeneral(), this.updateInfo.getSurnameS().getText())) {
-				
+					this.mainViewController.getRegexPassport(), this.updateInfo.getNameS().getText(),
+					this.mainViewController.getRegexGeneral(), this.updateInfo.getSurnameS().getText())) {
+
+				// GETTING HEAD FOR SEARCHING TARGET
 				Node tempNode = this.mainViewController.getMyList().getFirst();
 				int id = Integer.valueOf(this.updateInfo.getId().getText());
-				
+
+				// GETTING DATA FROM THE UPDATEINFO VIEW
+
+				// PASSPORT, NAME, SURNAME
 				int newPassport = Integer.valueOf(this.updateInfo.getPassport().getText());
 				String newName = this.updateInfo.getNameS().getText();
 				String newSurname = this.updateInfo.getSurnameS().getText();
-				
+
+				// DATE
 				Date tempDate = this.updateInfo.getDateOfArrival().getDate();
 				SimpleDateFormat myDateSimp = new SimpleDateFormat("dd/MM/yyyy");
 				String newDate = myDateSimp.format(tempDate);
-				
+
+				// PRIORITY
 				int tempPriorityInt = this.updateInfo.getPriority().getSelectedIndex();
 				String[] tempPriorityArrayStr = this.mainView.getPriorities();
 				String tempPriorityStr = tempPriorityArrayStr[tempPriorityInt].toUpperCase();
 				Priority tempPriority = Priority.valueOf(tempPriorityStr);
-				
-				JOptionPane.showMessageDialog(this.mainView, this.mainView.addLabelOpt("The Candidate's information has been UPDATED!"));
-				
-				while (tempNode.getCandidate().getID() != id ) {
-					tempNode = tempNode.getNext();				
+
+				JOptionPane.showMessageDialog(this.mainView,
+						this.mainView.addLabelOpt("The Candidate's information has been UPDATED!"));
+
+				// SEARCH TARGET STARTING FROM THE HEAD
+				while (tempNode.getCandidate().getID() != id) {
+					tempNode = tempNode.getNext();
 				}
-							
+
+				// IF THE PRIORITY IS UPDATE, THE NODE IS GOING TO BE DELETE
 				if (tempNode.getPriority() != tempPriority) {
-					
-					this.mainViewController.getMyList().deleteNode(tempNode);				
+
+					this.mainViewController.getMyList().deleteNode(tempNode);
 					Candidate tempCandidate = new Candidate(newName, newSurname, newDate, newPassport, id);
+					// CREATING A NOW NODE WITH A NEW PRIORITY
 					Node tempNodeTwo = new Node(tempCandidate, tempPriority);
 					this.mainViewController.getMyList().insertNode(tempNodeTwo);
 				} else {
@@ -73,13 +86,14 @@ public class UpdateInfoController implements ActionListener {
 					tempNode.getCandidate().setSurname(newSurname);
 					tempNode.getCandidate().setDateOfArribal(newDate);
 				}
-							
+
+				// UPDATING INFORMATION IN MAIN VIEW
 				String[][] data = this.mainViewController.getMyList().convertToArrayLinkedList();
 				this.mainView.setData(data);
 				this.mainView.updateView();
 				this.updateInfo.dispose();
-			}			
-			
+			}
+
 		}
 
 	}
